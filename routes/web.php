2f3bb3557a614670da3ca8f91b59e7cd3d080b7f
Rick Simon <rick.rf.simon@gmail.com>
2018-03-14 23:16:06 -0700

    preliminary registration rebrand

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');  //TODO: can probably remove this (use registration page)
